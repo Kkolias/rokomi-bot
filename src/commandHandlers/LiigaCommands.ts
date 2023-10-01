072dc91c837822969b5d24dc9e790a5062ed63d7
Elias Koivuporras <eppu2002@gmail.com>
2023-10-01 19:32:54 +0300

    utils

---
+++ src/commandHandlers/LiigaCommands.ts
@@ -0,0 +1,40 @@
+import { parseISODate } from "../utils/time";
+import liigaUtil from "../utils/Liiga";
+import { LiigaGame } from "interfaces/liiga";
+
+export class LiigaCommandHandler {
+  async liigaToday(): Promise<string> {
+    const allGames = await liigaUtil.getSeasonLiigaGames();
+    const todayGames = liigaUtil.getTodaysLiigaGames(allGames);
+    const parsedGames = liigaUtil.parseTeamsFromGames(todayGames);
+
+    if (parsedGames?.length) {
+      const output = parsedGames?.map((g) => `${g}\n`).join("");
+      return output;
+    }
+
+    return this.findNextGames(allGames);
+  }
+
+  private findNextGames(allGames: LiigaGame[]): string {
+    const nextGames = liigaUtil.getNextGameDayLiigaGames(
+      allGames,
+      parseISODate(new Date())
+    );
+
+    if (nextGames?.length) {
+      const parsedGames = liigaUtil.parseTeamsFromGames(nextGames);
+      const parsedOutput = parsedGames?.map((g) => `${g}\n`);
+      const withPrefix = [
+        "Ei liigapelejä tänään. Seuraavat pelit: \n",
+        ...parsedOutput,
+      ];
+      const output = withPrefix.join("");
+      return output;
+    } else {
+      return "vedä käteen";
+    }
+  }
+}
+
+export default new LiigaCommandHandler();
---
+++ src/enums/liiga.enum.ts
@@ -0,0 +1,5 @@
+export enum LiigaSeries {
+    RUNKOSARJA = 'RUNKOSARJA',
+    CHL = 'CHL',
+    PLAYOFFS = 'PLAYOFFS'
+}
